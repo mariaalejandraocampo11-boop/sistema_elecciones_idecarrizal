80e22c27e6c76fa7f6c40fa6a0be0628be079d3e
mariaalejandraocampo11-boop <mariaalejandraocampo11@gmail.com>
2025-08-22 18:24:47 -0300

    se inicio el proyecto de elecciones
    1 se agrego el index
    2 se agrego el carrusel
    3 se creo una carpeta(header)
    4 se agregaron 3 imagenes

---
+++ js/header.js
@@ -0,0 +1,9 @@
+document.querySelector('.encabezado-principal').innerHTML = `
+    <h1>IED NOBEL JUAN MANUEL SANTOS</h1>
+    <nav>
+        <a href="index.html">Inicio</a>
+        <a href="registrar_estudiante.html">Estudiantes</a>
+        <a href="contacto.html">Contacto</a>
+        <a href="login.html">Login</a>
+    </nav>
+`;
