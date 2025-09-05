0fbcbe472c1a06a1db44f36c430bf3f046aa4246
mariaalejandraocampo11-boop <mariaalejandraocampo11@gmail.com>
2025-09-05 17:53:05 -0300

    proyecto productivo

--- js/header.js
+++ js/header.js
@@ -1,9 +1,9 @@
 document.querySelector('.encabezado-principal').innerHTML = `
-    <h1>IED NOBEL JUAN MANUEL SANTOS</h1>
+    <h1>IED TECNICO DE CARRIZAL </h1>
     <nav>
         <a href="index.html">Inicio</a>
         <a href="registrar_estudiante.html">Estudiantes</a>
         <a href="contacto.html">Contacto</a>
         <a href="login.html">Login</a>
     </nav>
-`;
+`
